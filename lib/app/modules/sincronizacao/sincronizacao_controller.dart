6467b48734ffc0baf8950bf4829068f46ab1bca5
Ita Chesterton win <itajacy@gmail.com>
2024-10-03 19:59:36 -0300

    correcoes no sincronizacaoController, esta alterando todos os clientes para clientes em branco, rever o codigo

--- lib/app/modules/sincronizacao/sincronizacao_controller.dart
+++ lib/app/modules/sincronizacao/sincronizacao_controller.dart
@@ -3,9 +3,15 @@ import '../../services/database/dao/clientes_dao.dart';
 
 class SincronizacaoController {
   ClientesDao clientesDao = ClientesDao();
-
+  int resposta = 0;
   Future<int> lerArquivoESalvarClientes(Cliente cliente) async {
-    int resposta = await clientesDao.salvar(cliente);
+    List resp = await clientesDao.findClientes(cliente.razaoSocial!);
+
+    if (resp.length > 0) {
+      resposta = await clientesDao.alterar(cliente);
+    } else {
+      resposta = await clientesDao.salvar(cliente);
+    }
     return resposta;
   }
 
--- lib/app/modules/sincronizacao/sincronizacao_page.dart
+++ lib/app/modules/sincronizacao/sincronizacao_page.dart
@@ -15,14 +15,11 @@ import 'package:xml2json/xml2json.dart';
 
 import '../../core/colors_app.dart';
 import '../../core/strings.dart';
-import '../../services/database/dao/clientes_dao.dart';
 import '../configuracao/widgets/directory_path.dart';
 
 class SincronizacaoPage extends StatelessWidget {
   SincronizacaoPage({super.key});
 
-  //!ClientesDao clientesDao = ClientesDao();
-  // Cliente novoCliente = Cliente();
   SincronizacaoController sincronizacaoClientesController =
       SincronizacaoController();
 
@@ -70,10 +67,6 @@ class SincronizacaoPage extends StatelessWidget {
             children: [
               GeneralIconButton(
                 onPress: () async {
-                  //!  APAGANDO TODOS OS CLIENTES
-                  print('=========APAGANDO TODOS OS CLIENTES');
-                  sincronizacaoClientesController.apagaTodosOsClientes();
-                  print('=========TODOS OS CLIENTES APAGADOS');
 
                   //! =========== INICIO TESTE DO XML2JSON
 
@@ -96,101 +89,6 @@ class SincronizacaoPage extends StatelessWidget {
                   //* convertendo bytes para String
                   String xmlString = String.fromCharCodes(xmlBytes);
 
-//! PARA VOLTAR A LER O ARQUIVO BASTA APAGAR A VARIAVEL xmlString abaixo ;
-//                   xmlString = '''
-// <DataSet>
-//   <Row>
-//     <ClienteID_Int>0001-00</ClienteID_Int>
-//     <DispositivoID>0</DispositivoID>
-//     <ClienteID_Mob>8819</ClienteID_Mob>
-//     <TipoPessoa>J</TipoPessoa>
-//     <CGCCPF>07.237.672/0001-64</CGCCPF>
-//     <IERG>282121420113</IERG>
-//     <RazaoSocial>CURURU MARIA BABBONI - ME.</RazaoSocial>
-//     <NomeFantasia>CURURU MARIA BABBONI - ME.</NomeFantasia>
-//     <Contato>CURURU</Contato>
-//     <FoneCom1>012-3144 4266</FoneCom1>
-//     <FoneCom2/>
-//     <FoneCel/>
-//     <FoneRes/>
-//     <FoneFax/>
-//     <Email>babboni@itelefonica.com.br</Email>
-//     <S_Endereco>R.CAPITAO NECO, 178</S_Endereco>
-//     <S_Complemento/>
-//     <S_Bairro>CENTRO</S_Bairro>
-//     <S_Cidade>CRUZEIRO</S_Cidade>
-//     <S_UF>SP</S_UF>
-//     <S_CEP>12.701-350</S_CEP>
-//     <E_Endereco>R.CAPITAO NECO, 178</E_Endereco>
-//     <E_Complemento/>
-//     <E_Bairro>CENTRO</E_Bairro>
-//     <E_Cidade>CRUZEIRO</E_Cidade>
-//     <E_UF>SP</E_UF>
-//     <E_CEP>12.701-350</E_CEP>
-//   </Row>
-//   <Row>
-//     <ClienteID_Int>0002-00</ClienteID_Int>
-//     <DispositivoID>0</DispositivoID>
-//     <ClienteID_Mob>6692</ClienteID_Mob>
-//     <TipoPessoa>J</TipoPessoa>
-//     <CGCCPF>71.951.149/0001-02</CGCCPF>
-//     <IERG>392.073.527.110</IERG>
-//     <RazaoSocial>ELIANA ANGELA RAMOS BIONDI ME</RazaoSocial>
-//     <NomeFantasia>ELIANA ANGELA RAMOS BIONDI ME</NomeFantasia>
-//     <Contato>PEDRO</Contato>
-//     <FoneCom1>012-3951 9792</FoneCom1>
-//     <FoneCom2/>
-//     <FoneCel/>
-//     <FoneRes/>
-//     <FoneFax/>
-//     <Email/>
-//     <S_Endereco>R.CHAQUIB SLEIMAN AHMED, 13</S_Endereco>
-//     <S_Complemento/>
-//     <S_Bairro>JD.PEREIRA DO AMPARO</S_Bairro>
-//     <S_Cidade>JACAREI</S_Cidade>
-//     <S_UF>SP</S_UF>
-//     <S_CEP>12.327-700</S_CEP>
-//     <E_Endereco>R.CHAQUIB SLEIMAN AHMED, 13</E_Endereco>
-//     <E_Complemento/>
-//     <E_Bairro>JD.PEREIRA DO AMPARO</E_Bairro>
-//     <E_Cidade>JACAREI</E_Cidade>
-//     <E_UF>SP</E_UF>
-//     <E_CEP>12.327-700</E_CEP>
-//   </Row>
-//   <Row>
-//     <ClienteID_Int>0003-00</ClienteID_Int>
-//     <DispositivoID>0</DispositivoID>
-//     <ClienteID_Mob>6693</ClienteID_Mob>
-//     <TipoPessoa>J</TipoPessoa>
-//     <CGCCPF>00.574.549/0001-90</CGCCPF>
-//     <IERG>392.081.120.117</IERG>
-//     <RazaoSocial>BIONDI MARTINS LTDA</RazaoSocial>
-//     <NomeFantasia>BIONDI MARTINS LTDA</NomeFantasia>
-//     <Contato/>
-//     <FoneCom1/>
-//     <FoneCom2/>
-//     <FoneCel/>
-//     <FoneRes/>
-//     <FoneFax/>
-//     <Email/>
-//     <S_Endereco>PC.INDEPENDENCIA, 168</S_Endereco>
-//     <S_Complemento/>
-//     <S_Bairro>JD.INDEPENDENCIA</S_Bairro>
-//     <S_Cidade>JACAREI</S_Cidade>
-//     <S_UF>SP</S_UF>
-//     <S_CEP>12.300-000</S_CEP>
-//     <E_Endereco>PC.INDEPENDENCIA, 168</E_Endereco>
-//     <E_Complemento/>
-//     <E_Bairro>JD.INDEPENDENCIA</E_Bairro>
-//     <E_Cidade>JACAREI</E_Cidade>
-//     <E_UF>SP</E_UF>
-//     <E_CEP>12.300-000</E_CEP>
-//   </Row>
-// </DataSet>
-// ''';
-
-                  // print('xmlString--> $xmlString');
-
                   //* Criação de uma instância do converter XML para JSON
                   Xml2Json xml2json = Xml2Json();
                   // print(
@@ -211,138 +109,30 @@ class SincronizacaoPage extends StatelessWidget {
 
                   int totalClientes = mapCLientes['DataSet']['Row'].length;
 
-                  //! tentativa de salvar os clientes
-                  // for (var i = 0; i < totalClientes; i++) {
-
-                  //!---------------tentando com Map---------------------
-                  // novoCliente.clienteId = 0;
-                  // novoCliente.clienteIdInt =
-                  //     mapCLientes['DataSet']['Row']['ClienteID_Int'][i];
-
-                  // novoCliente.dispositivoId = mapCLientes['DataSet']['Row']
-                  //     ['DispositivoID'][i] as String;
-                  // novoCliente.clienteIdMob = mapCLientes['DataSet']['Row']
-                  //     ['ClienteID_Mob'][i] as String;
-                  // novoCliente.tipoPessoa = mapCLientes['DataSet']['Row']
-                  //     ['TipoPessoa'][i] as String;
-                  // novoCliente.cNPJCPF =
-                  //     mapCLientes['DataSet']['Row']['CGCCPF'][i] as String;
-                  // novoCliente.iERG =
-                  //     mapCLientes['DataSet']['Row']['IERG'][i] as String;
-                  // novoCliente.razaoSocial = mapCLientes['DataSet']['Row']
-                  //     ['RazaoSocial'][i] as String;
-                  // novoCliente.nomeFantasia = mapCLientes['DataSet']['Row']
-                  //     ['NomeFantasia'][i] as String;
-                  // novoCliente.contato =
-                  //     mapCLientes['DataSet']['Row']['Contato'][i] as String;
-                  // novoCliente.fone1 =
-                  //     mapCLientes['DataSet']['Row']['FoneCom1'][i] as String;
-                  // novoCliente.fone2 =
-                  //     mapCLientes['DataSet']['Row']['FoneCom2'][i] as String;
-                  // novoCliente.foneCel =
-                  //     mapCLientes['DataSet']['Row']['FoneCel2'][i] as String;
-                  // novoCliente.foneRes =
-                  //     mapCLientes['DataSet']['Row']['FoneRes'][i] as String;
-                  // novoCliente.fax =
-                  //     mapCLientes['DataSet']['Row']['FoneFax'][i] as String;
-                  // novoCliente.email =
-                  //     mapCLientes['DataSet']['Row']['Email'][i] as String;
-                  // novoCliente.pEndereco = mapCLientes['DataSet']['Row']
-                  //     ['S_Endereco'][i] as String;
-                  // novoCliente.pComplemento = mapCLientes['DataSet']['Row']
-                  //     ['S_Complemento'][i] as String;
-                  // novoCliente.pBairro =
-                  //     mapCLientes['DataSet']['Row']['S_Bairro'][i] as String;
-                  // novoCliente.pCidade =
-                  //     mapCLientes['DataSet']['Row']['S_Cidade'][i] as String;
-                  // novoCliente.pUF =
-                  //     mapCLientes['DataSet']['Row']['S_UF'][i] as String;
-                  // novoCliente.pCEP =
-                  //     mapCLientes['DataSet']['Row']['S_CEP'][i] as String;
-                  // novoCliente.eEndereco = mapCLientes['DataSet']['Row']
-                  //     ['E_Endereco'][i] as String;
-                  // novoCliente.eComplemento = mapCLientes['DataSet']['Row']
-                  //     ['E_Complemento'][i] as String;
-                  // novoCliente.eBairro =
-                  //     mapCLientes['DataSet']['Row']['E_Bairro'][i] as String;
-                  // novoCliente.eCidade =
-                  //     mapCLientes['DataSet']['Row']['E_Cidade'][i] as String;
-                  // novoCliente.eUF =
-                  //     mapCLientes['DataSet']['Row']['E_UF'][i] as String;
-                  // novoCliente.eCEP =
-                  //     mapCLientes['DataSet']['Row']['E_CEP'][i] as String;
-
-                  //!------------------------------------
-                  //! int resposta = await clientesDao.salvar(novoCliente);
-                  // print('resposta --> $resposta');
-                  // }
-
                   //* Cria um List dos Maps
-                  List clientesList = [];
+                  List clientesListMap = [];
 
                   for (var element = 0; element < totalClientes; element++) {
-                    clientesList.add((mapCLientes['DataSet']['Row'][element]));
+                    clientesListMap
+                        .add((mapCLientes['DataSet']['Row'][element]));
                   }
 
-                  for (var elemento in clientesList) {
-                    Cliente novoCliente = Cliente();
+                  final List<Cliente> clienteListaObjeto = List<Cliente>.from(
+                      clientesListMap.map((model) => Cliente.fromMap(model)));
 
-                    //* ============com List==============================
-
-                    // novoCliente.clienteId = 0;
-                    novoCliente.clienteIdInt = elemento['ClienteID_Int'];
-
-                    novoCliente.dispositivoId = elemento['DispositivoID'];
-                    novoCliente.clienteIdMob = elemento['ClienteID_Mob'];
-                    novoCliente.tipoPessoa = elemento['TipoPessoa'];
-                    novoCliente.cNPJCPF = elemento['CGCCPF'];
-                    novoCliente.iERG = elemento['IERG'];
-                    novoCliente.razaoSocial = elemento['RazaoSocial'];
-                    novoCliente.nomeFantasia = elemento['NomeFantasia'];
-                    novoCliente.contato = elemento['Contato'];
-                    novoCliente.fone1 = elemento['FoneCom1'];
-                    novoCliente.fone2 = elemento['FoneCom2'];
-                    novoCliente.foneCel = elemento['FoneCel2'];
-                    novoCliente.foneRes = elemento['FoneRes'];
-                    novoCliente.fax = elemento['FoneFax'];
-                    novoCliente.email = elemento['Email'];
-                    novoCliente.pEndereco = elemento['S_Endereco'];
-                    novoCliente.pComplemento = elemento['S_Complemento'];
-                    novoCliente.pBairro = elemento['S_Bairro'];
-                    novoCliente.pCidade = elemento['S_Cidade'];
-                    novoCliente.pUF = elemento['S_UF'];
-                    novoCliente.pCEP = elemento['S_CEP'];
-                    novoCliente.eEndereco = elemento['E_Endereco'];
-                    novoCliente.eComplemento = elemento['E_Complemento'];
-                    novoCliente.eBairro = elemento['E_Bairro'];
-                    novoCliente.eCidade = elemento['E_Cidade'];
-                    novoCliente.eUF = elemento['E_UF'];
-                    novoCliente.eCEP = elemento['E_CEP'];
-
-                    // print(novoCliente);
+                  for (var elemento in clienteListaObjeto) {
                     print('-=-=-=-=adicionando novo cliente-=-=-=-=');
 
                     int resposta = await sincronizacaoClientesController
-                        .lerArquivoESalvarClientes(novoCliente);
+                        .lerArquivoESalvarClientes(elemento);
                     //! int resposta = await clientesDao.salvar(novoCliente);
                     print('resposta --> $resposta');
-
-                    //* ==========================================
-
-                    // print('============');
-                    // print(elemento['NomeFantasia']);
-                    // print(
-                    //     '${elemento['ClienteID_Int'].toString()}  ${elemento['NomeFantasia'].toString()}');
                     print('============');
                   }
 
-                  print('Total de Clientes--> ${clientesList.length}');
-
-                  // clientesList.forEach(print);
+                  print('Total de Clientes--> ${clienteListaObjeto.length}');
 
                   print('--------------------fim----------------------------');
-
-                  // List clientesLIst = mapClientes['DataSet']['Row'];
                 },
                 icone: Icons.sync_outlined,
                 iconSize: Sizes.sizeH_30,
