38f58f212d4ab0d4424052abac46d3ccd97f4fb6
Ita Chesterton win <itajacy@gmail.com>
2024-10-29 19:44:53 -0300

    configuracaoFTP mensagem de conexao em andamento, funcionou

--- lib/app/modules/configuracao/configuracao_ftp_controller.dart
+++ lib/app/modules/configuracao/configuracao_ftp_controller.dart
@@ -7,66 +7,21 @@ class ConfiguracaoFtpController extends GetxController {
 
   testeConexaoFTP(String endereco, String usuario, String senha) async {
     FTPConnect ftpConnect = FTPConnect(endereco, user: usuario, pass: senha);
-    // String fileName = 'arq.zip';
-    // var getPathFile = DirectoryPath();
-    // var storePath = await getPathFile.getPath();
-    // String filePath = '$storePath/$fileName';
 
-    // var arquivo = File(filePath);
-    // print('arquivo--> $arquivo');
     try {
+      mensagem = 'Aguarde, conexão em andamento...';
+      update();
       // Conectando ao servidor
       conectado = await ftpConnect.connect();
       update();
 
-      // Obtendo o tamanho do arquivo remoto
-      // int fileSize = await ftpConnect.sizeFile(fileName);
-      // print('Tamanho do arquivo no FTP: $fileSize bytes');
-
-      // Garantindo que o arquivo esteja vazio antes do download
-      // if (arquivo.existsSync()) {
-      //   print('apagando arquivo--> $arquivo');
-      //   print("arq.zip existe..> ${arquivo.existsSync()}");
-      //   arquivo.deleteSync();
-      //   print("arq.zip existe..> ${arquivo.existsSync()}");
-      //   print('arquivo apagado!!!');
-      // }
-      // arquivo.createSync();
-
-      // Baixando o arquivo diretamente para o armazenamento
-      // await ftpConnect.downloadFileWithRetry(
-      //   fileName,
-      //   arquivo,
-      //   pRetryCount: 2,
-      //   onProgress: (progressInPercent, totalReceived, fileSize) {
-      //     // Atualizando o progresso do download
-      //     setState(() {
-      //       _downloadProgress = progressInPercent / 100;
-      //       conectado;
-      //     });
-      //     print('Progresso: ${progressInPercent.toStringAsFixed(2)}%');
-      //     print('Total recebido: $totalReceived de $fileSize bytes');
-      //   },
-      // );
-
-      // print('Download concluído com sucesso!');
-
-      // Verificando o tamanho do arquivo baixado
-      // int localFileSize = arquivo.lengthSync();
-      // print('Tamanho do arquivo baixado: $localFileSize bytes');
-
-      // if (localFileSize != fileSize) {
-      //   print('Erro: O arquivo baixado tem um tamanho diferente do original.');
-      // }
-
       mensagem = 'CONECTADO COM SUCESSO';
       update();
-      print(mensagem);
 
       // Desconectando do servidor
       await ftpConnect.disconnect();
     } catch (e) {
-      //! FTPConnectException: Wrong Username/password (Response: 530 User or password not found)
+      //* FTPConnectException: Wrong Username/password (Response: 530 User or password not found)
       conectado = false;
       if (e.toString().contains('530')) {
         mensagem = 'Usuário e/ou Senha inválido(s)';
@@ -74,9 +29,6 @@ class ConfiguracaoFtpController extends GetxController {
         mensagem = 'Falha ao conectar:  ${e.toString()}';
       }
       update();
-      print(mensagem);
-
-      // print('Erro: $e');
     }
   }
 }
